77434ef28d5039954967b1a370cc192a595f91a5
LorenzoW <kappaquindici@gmail.com>
2013-05-28 14:50:04 +0200

    first commit

---
+++ gen/eu/fbk/notifiche_georeferenziate/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package eu.fbk.notifiche_georeferenziate;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/eu/fbk/notifiche_georeferenziate/ListenerPosition.java
@@ -0,0 +1,24 @@
+package eu.fbk.notifiche_georeferenziate;
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+public class ListenerPosition extends CordovaPlugin {
+	 @Override
+	    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	        if (action.equals("echo")) {
+	            String message = args.getString(0); 
+	            this.echo(message, callbackContext);
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    private void echo(String message, CallbackContext callbackContext) {
+	        if (message != null && message.length() > 0) { 
+	            callbackContext.success(message);
+	        } else {
+	            callbackContext.error("Expected one non-empty string argument.");
+	        }
+	    }
+}
